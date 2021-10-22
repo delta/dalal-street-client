d5e1020407d297d03a2aca85e2bd3ea7b366a2ae
siva2204 <sivaraman26r@gmail.com>
2021-10-22 23:52:41 +0530

    [done] grpc setup done

--- build_proto.sh
+++ build_proto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf proto_build/
-mkdir proto_build
+rm -rf ./lib/proto_build
+mkdir ./lib/proto_build
 
-protoc --proto_path=proto/ --dart_out=grpc:proto_build proto/*.proto
-protoc --proto_path=proto/ --dart_out=grpc:proto_build proto/actions/*.proto
-protoc --proto_path=proto/ --dart_out=grpc:proto_build proto/datastreams/*.proto
-protoc --proto_path=proto/ --dart_out=grpc:proto_build proto/models/*.proto
+protoc --proto_path=proto/ --dart_out=grpc:lib/proto_build proto/*.proto
+protoc --proto_path=proto/ --dart_out=grpc:lib/proto_build proto/actions/*.proto
+protoc --proto_path=proto/ --dart_out=grpc:lib/proto_build proto/datastreams/*.proto
+protoc --proto_path=proto/ --dart_out=grpc:lib/proto_build proto/models/*.proto
