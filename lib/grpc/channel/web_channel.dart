89360a81145de4f79e37260913f05311395b16ae
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2021-11-09 16:16:36 +0530

    feat(config): add web and mobile configs in the same json file

--- lib/grpc/channel/web_channel.dart
+++ lib/grpc/channel/web_channel.dart
@@ -3,6 +3,6 @@ import 'package:grpc/grpc_connection_interface.dart';
 import 'package:grpc/grpc_web.dart';
 
 /// [ClientChannel] for web platform
-final _webChannel = GrpcWebClientChannel.xhr(Uri.parse(grpcConfig.url));
+final _webChannel = GrpcWebClientChannel.xhr(Uri.parse(webConfig.url));
 
 Future<ClientChannelBase> getChannel() async => _webChannel;
