20086ff6971167bc0f8b1d53c049f8885dfbd1ce
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2021-11-11 16:40:32 +0530

    feat(docs): add documentation for getIt

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,41 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
+/// ## Dependency Injection
+///
+/// ### Registering objects
+/// #### Register objects to then read them from anywhere in the project
+/// ```dart
+/// // Most of the time getIt can differentiate between objects from thier Type
+/// getIt.registerSingleton<String>(sessionId);
+/// // Or like this, as type can be inferred in dart
+/// getIt.registerSingleton(sessionId);
+///
+/// // But if you have multiple objects of same type, then mention a name
+/// getIt.registerSingleton(sessionId);
+/// getIt.registerSingleton(someOtherString, instanceName: 'String 2');
+/// ```
+///
+/// ### Reading objects
+/// #### Warning: Trying to read unregistered objects will throw exception
+/// ```dart
+/// final sessionId = getIt<String>();
+/// // or
+/// final String sessionId = getIt();
+/// // reading objects registered with a name
+/// final String string2 = getIt(instanceName: 'String 2');
+/// ```
+///
+/// ### Unregistering objects
+/// ```dart
+/// // Unregister objects with thier type
+/// getIt.unregister<String>();
+/// getIt.unregister<User>();
+/// // Can unregister with names
+/// getIt.unregister(instanceName: 'String 2');
+/// ```
+/// 
+/// For more complex usecases checkout [get_it](https://pub.dev/packages/get_it)
 final GetIt getIt = GetIt.I;
 
 void main() async {
