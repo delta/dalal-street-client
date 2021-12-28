37c46f32d52cf29505fc1fa25905beaab2ff10f7
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2021-12-28 17:40:36 +0530

    [refactor] [exchange]: modify exchange page to use global streams

--- lib/blocs/exchange/exchange_cubit.dart
+++ lib/blocs/exchange/exchange_cubit.dart
@@ -2,29 +2,47 @@ import 'package:bloc/bloc.dart';
 import 'package:dalal_street_client/config/get_it.dart';
 import 'package:dalal_street_client/config/log.dart';
 import 'package:dalal_street_client/constants/error_messages.dart';
+import 'package:dalal_street_client/global_streams.dart';
 import 'package:dalal_street_client/grpc/client.dart';
 import 'package:dalal_street_client/proto_build/actions/BuyStocksFromExchange.pb.dart';
+import 'package:dalal_street_client/proto_build/datastreams/StockExchange.pb.dart';
+import 'package:dalal_street_client/proto_build/models/Stock.pb.dart';
+import 'package:dalal_street_client/utils/convert.dart';
 import 'package:equatable/equatable.dart';
 import 'package:fixnum/fixnum.dart';
 
 part 'exchange_state.dart';
 
 class ExchangeCubit extends Cubit<ExchangeState> {
-  ExchangeCubit() : super(ExchangeInitial());
+  ExchangeCubit() : super(const ExchangeLoading());
+
+  Future<void> listenToExchangeStream(Map<int, Stock> mapOfStocks) async {
+    // Load initial state
+    emit(ExchangeDataLoaded(mapOfStocks
+        .map((key, value) => MapEntry(key, stockDataToExchangeData(value)))));
+    final exchangeStream = getIt<GlobalStreams>().stockExchangeStream;
+    await for (var item in exchangeStream) {
+      final exchangeData = item.stocksInExchange;
+      logger.d(exchangeData);
+      // Load new updates
+      emit(ExchangeDataLoaded(exchangeData));
+    }
+  }
 
   Future<void> buyStocksFromExchange(int stockId, int stockQuantity) async {
-    emit(const ExchangeLoading());
+    // TODO: Show a progress dialog
     try {
       final resp = await actionClient.buyStocksFromExchange(
           BuyStocksFromExchangeRequest(
-              stockId: stockId, stockQuantity: Int64(stockQuantity)),
+            stockId: stockId,
+            stockQuantity: Int64(stockQuantity),
+          ),
           options: sessionOptions(getIt<String>()));
       if (resp.statusCode == BuyStocksFromExchangeResponse_StatusCode.OK) {
-        emit(ExchangeSuccess(resp));
+        emit(BuyFromExchangeSuccess(resp));
       } else {
         logger.e(resp.statusMessage);
         emit(ExchangeFailure(resp.statusMessage));
-        emit(ExchangeInitial());
       }
     } catch (e) {
       logger.e(e);
--- lib/pages/exchange_page.dart
+++ lib/pages/exchange_page.dart
@@ -1,15 +1,13 @@
-import 'package:dalal_street_client/blocs/companies/companies_bloc.dart';
-import 'package:dalal_street_client/blocs/exchange/exchangeStream/exchange_stream_bloc.dart';
 import 'package:dalal_street_client/blocs/exchange/exchange_cubit.dart';
-import 'package:dalal_street_client/blocs/subscribe/subscribe_cubit.dart';
+import 'package:dalal_street_client/config/get_it.dart';
 import 'package:dalal_street_client/config/log.dart';
-import 'package:dalal_street_client/proto_build/datastreams/Subscribe.pb.dart';
+import 'package:dalal_street_client/global_streams.dart';
+import 'package:dalal_street_client/proto_build/datastreams/StockExchange.pb.dart';
 import 'package:dalal_street_client/proto_build/models/Stock.pb.dart';
 import 'package:dalal_street_client/theme/buttons.dart';
 import 'package:dalal_street_client/theme/colors.dart';
-import 'package:dalal_street_client/utils/responsive.dart';
+import 'package:dalal_street_client/utils/convert.dart';
 import 'package:dalal_street_client/utils/snackbar.dart';
-import 'package:fixnum/fixnum.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
@@ -25,28 +23,12 @@ class ExchangePage extends StatefulWidget {
 }
 
 class _ExchangePageState extends State<ExchangePage> {
-  Map<int, Stock> mapOfStocks = {};
-  // Unsubscribe to the streams when the widget is disposed
-  @override
-  void dispose() {
-    SubscriptionId? _stockPricesExchangeSubscriptionId;
-    final state = context.read<SubscribeCubit>().state;
-    if (state is SubscriptionDataLoaded) {
-      _stockPricesExchangeSubscriptionId = state.subscriptionId;
-      context
-          .read<SubscribeCubit>()
-          .unsubscribe(_stockPricesExchangeSubscriptionId);
-    }
-    super.dispose();
-  }
+  Map<int, Stock> mapOfStocks = getIt<GlobalStreams>().stockList;
 
   @override
   void initState() {
     super.initState();
-    // Get List of Stocks
-    context.read<CompaniesBloc>().add(const GetStockList());
-    // Subscribe to the stream of Stock Exchange Update
-    context.read<SubscribeCubit>().subscribe(DataStreamType.STOCK_EXCHANGE);
+    context.read<ExchangeCubit>().listenToExchangeStream(mapOfStocks);
   }
 
   @override
@@ -72,138 +54,148 @@ class _ExchangePageState extends State<ExchangePage> {
         ),
       );
 
-  Widget _buildBody() {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Stock Exchange',
-            style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-                color: whiteWithOpacity75),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            'Companies',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.w700,
-              color: white,
-            ),
-            textAlign: TextAlign.start,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          _exchangeBody(),
-        ],
-      ),
-    );
-  }
+  // Container _companiesExchange() => Container(
+  //       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+  //       decoration: BoxDecoration(
+  //         color: backgroundColor,
+  //         borderRadius: BorderRadius.circular(10),
+  //       ),
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.start,
+  //         crossAxisAlignment: CrossAxisAlignment.start,
+  //         children: [
+  //           const Text(
+  //             'Stock Exchange',
+  //             style: TextStyle(
+  //                 fontSize: 18,
+  //                 fontWeight: FontWeight.w500,
+  //                 color: whiteWithOpacity75),
+  //             textAlign: TextAlign.center,
+  //           ),
+  //           const SizedBox(
+  //             height: 20,
+  //           ),
+  //           const Text(
+  //             'Companies',
+  //             style: TextStyle(
+  //               fontSize: 22,
+  //               fontWeight: FontWeight.w700,
+  //               color: white,
+  //             ),
+  //             textAlign: TextAlign.start,
+  //           ),
+  //           const SizedBox(
+  //             height: 20,
+  //           ),
+  //           _exchangeBody(),
+  //         ],
+  //       ),
+  //     );
 
-  Widget _exchangeBody() =>
-      BlocBuilder<CompaniesBloc, CompaniesState>(builder: (context, state) {
-        if (state is GetCompaniesSuccess) {
-          mapOfStocks = state.stockList.stockList;
-          return BlocBuilder<SubscribeCubit, SubscribeState>(
-              builder: (context, state) {
-            if (state is SubscriptionDataLoaded) {
-              // Start the stream of Stock Prices
-              context
-                  .read<ExchangeStreamBloc>()
-                  .add(SubscribeToStockExchange(state.subscriptionId));
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: mapOfStocks.length,
-                itemBuilder: (context, index) {
-                  Stock? company = mapOfStocks[index + 1];
-                  int currentPrice = company?.currentPrice.toInt() ?? 0;
-                  int previousDayPrice = company?.previousDayClose.toInt() ?? 0;
-                  var priceChange = (currentPrice - previousDayPrice);
-                  return _exchangeListItem(
-                      company, index + 1, priceChange, currentPrice);
-                },
+  Widget _exchangeBody() => BlocBuilder<ExchangeCubit, ExchangeState>(
+        builder: (context, state) {
+          if (state is ExchangeDataLoaded) {
+            final exchangeData = state.exchangeData;
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: mapOfStocks.length,
+              itemBuilder: (context, index) {
+                Stock? company = mapOfStocks[index + 1];
+                int currentPrice = exchangeData[index + 1]?.price.toInt() ?? 0;
+                int previousDayPrice = company?.previousDayClose.toInt() ?? 0;
+                var priceChange = (currentPrice - previousDayPrice);
+                return _stockExchangeItem(
+                  company,
+                  index + 1,
+                  priceChange,
+                  currentPrice,
+                  exchangeData[index + 1] ??
+                      stockDataToExchangeData(mapOfStocks[index + 1]!),
+                );
+              },
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      );
+
+  Widget _stockExchangeItem(
+    Stock? company,
+    int index,
+    int priceChange,
+    int currentPrice,
+    StockExchangeDataPoint exchangeDataPoint,
+  ) =>
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              _stockNames(company),
+              _stockGraph(),
+              _stockPrices(
+                index,
+                priceChange,
+                currentPrice,
+                exchangeDataPoint,
+              ),
+            ]),
+            const SizedBox(
+              height: 10,
+            ),
+            () {
+              logger.i('$index here');
+              int stocksInMarket = exchangeDataPoint.stocksInMarket.toInt();
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Expanded(child: Text('Stocks in Market')),
+                  const SizedBox(width: 10),
+                  Text(stocksInMarket.toString())
+                ],
               );
-            } else if (state is SubscriptonDataFailed) {
-              logger.i('Stock Prices Stream Failed $state');
-              return const Center(
-                child: Text(
-                  'Failed to load data \nReason : //',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: secondaryColor,
+            }(),
+            const SizedBox(
+              height: 10,
+            ),
+            () {
+              int stocksInExchange = exchangeDataPoint.stocksInExchange.toInt();
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Expanded(child: Text('Stocks in Exchange')),
+                  const SizedBox(width: 10),
+                  Text(stocksInExchange.toString())
+                ],
+              );
+            }(),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    style: secondaryButtonStyle,
+                    onPressed: () {},
+                    child: const Text('View'),
                   ),
                 ),
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: secondaryColor,
+                const SizedBox(width: 20),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () => _showModalSheet(
+                        index, company?.fullName ?? 'Airtel', currentPrice),
+                    child: const Text('Buy'),
+                  ),
                 ),
-              );
-            }
-          });
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(
-              color: secondaryColor,
-            ),
-          );
-        }
-      });
-
-// Company list item widget for stock exchange
-  Widget _exchangeListItem(
-      Stock? company, int index, int priceChange, int currentPrice) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-          _stockNames(company),
-          _stockGraph(),
-          _stockPrices(index, priceChange, currentPrice),
-        ]),
-        const SizedBox(
-          height: 10,
-        ),
-        _stockExchangeDetails(index, company),
-        const SizedBox(
-          height: 10,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Expanded(
-              child: ElevatedButton(
-                style: secondaryButtonStyle,
-                onPressed: () {},
-                child: const Text('View'),
-              ),
-            ),
-            const SizedBox(width: 20),
-            Expanded(
-              child: ElevatedButton(
-                onPressed: () => _showModalSheet(
-                    index, company?.fullName ?? 'Airtel', currentPrice),
-                child: const Text('Buy'),
-              ),
-            ),
+              ],
+            )
           ],
-        )
-      ]),
-    );
-  }
+        ),
+      );
 
   Widget _stockNames(Stock? company) {
     return Expanded(
@@ -234,156 +226,39 @@ class _ExchangePageState extends State<ExchangePage> {
     );
   }
 
-  Widget _stockPrices(int index, int priceChange, int currentPrice) {
-    return Expanded(
-      child: BlocBuilder<ExchangeStreamBloc, ExchangeStreamState>(
-        builder: (context, state) {
-          if (state is SubscriptionToStockExchangeSuccess) {
-            int currentStockPrice = state
-                    .stockExchangeUpdate.stocksInExchange[index]?.price
-                    .toInt() ??
-                mapOfStocks[index]?.currentPrice.toInt() ??
-                0;
-
-            mapOfStocks[index]?.currentPrice = Int64(currentStockPrice);
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  oCcy.format(currentStockPrice).toString(),
-                  style: const TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  priceChange >= 0
-                      ? '+' + oCcy.format(priceChange).toString()
-                      : oCcy.format(priceChange).toString(),
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: priceChange > 0 ? secondaryColor : heartRed,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-              ],
-            );
-          } else if (state is SubscriptionToStockExchangeFailed) {
-            return const Center(
-              child: Text(
-                'Failed to load data \nReason : //',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: secondaryColor,
-                ),
-              ),
-            );
-          } else {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  oCcy.format(currentPrice).toString(),
-                  style: const TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  priceChange >= 0
-                      ? '+' + oCcy.format(priceChange).toString()
-                      : oCcy.format(priceChange).toString(),
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: priceChange > 0 ? secondaryColor : heartRed,
-                  ),
-                ),
-              ],
-            );
-          }
-        },
-      ),
-    );
-  }
-
-  Widget _stockExchangeDetails(
+  Expanded _stockPrices(
     int index,
-    Stock? company,
+    int priceChange,
+    int currentPrice,
+    StockExchangeDataPoint exchangeDataPoint,
   ) {
-    return BlocBuilder<ExchangeStreamBloc, ExchangeStreamState>(
-      builder: (context, state) {
-        if (state is SubscriptionToStockExchangeSuccess) {
-          int stocksInMarket = state
-                  .stockExchangeUpdate.stocksInExchange[index]?.stocksInMarket
-                  .toInt() ??
-              (mapOfStocks[index]?.stocksInMarket.toInt() ?? 0);
-          mapOfStocks[index]?.stocksInMarket = Int64(stocksInMarket);
-          int stocksInExchange = state
-                  .stockExchangeUpdate.stocksInExchange[index]?.stocksInExchange
-                  .toInt() ??
-              (mapOfStocks[index]?.stocksInExchange.toInt() ?? 0);
-          mapOfStocks[index]?.stocksInExchange = Int64(stocksInExchange);
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Expanded(child: Text('Stocks in Market')),
-                  const SizedBox(width: 10),
-                  Text(stocksInMarket.toString())
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Expanded(child: Text('Stocks in Exchange')),
-                  const SizedBox(width: 10),
-                  Text(stocksInExchange.toString())
-                ],
-              )
-            ],
-          );
-        } else if (state is SubscriptionToStockExchangeFailed) {
-          return const Center(
-            child: Text(
-              'Failed to load data \nReason : //',
-              style: TextStyle(
-                fontSize: 14,
-                color: secondaryColor,
+    return Expanded(
+      child: () {
+        int currentStockPrice = exchangeDataPoint.price.toInt();
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              oCcy.format(currentStockPrice).toString(),
+              style: const TextStyle(
+                fontSize: 18,
               ),
             ),
-          );
-        } else {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Expanded(child: Text('Stocks in Market')),
-                  const SizedBox(width: 10),
-                  Text(company?.stocksInMarket.toString() ?? '0')
-                ],
+            Text(
+              priceChange >= 0
+                  ? '+' + oCcy.format(priceChange).toString()
+                  : oCcy.format(priceChange).toString(),
+              style: TextStyle(
+                fontSize: 14,
+                color: priceChange > 0 ? secondaryColor : heartRed,
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Expanded(child: Text('Stocks in Exchange')),
-                  const SizedBox(width: 10),
-                  Text(company?.stocksInExchange.toString() ?? '0')
-                ],
-              )
-            ],
-          );
-        }
-      },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        );
+      }(),
     );
   }
 
@@ -401,7 +276,7 @@ class _ExchangePageState extends State<ExchangePage> {
             child: Padding(
               child: BlocConsumer<ExchangeCubit, ExchangeState>(
                 listener: (context, state) {
-                  if (state is ExchangeSuccess) {
+                  if (state is BuyFromExchangeSuccess) {
                     showSnackBar(
                         context, 'Successfully bought $stockName stocks');
                     Navigator.maybePop(context);
