f4ddb472fb23921c86030fc599177f9f5798634e
simmiggrwl <simmiggrwl@gmail.com>
2022-01-08 16:32:24 +0530

    fix: balance value in bottom sheet updated

--- lib/pages/company_page/company_page.dart
+++ lib/pages/company_page/company_page.dart
@@ -1,6 +1,7 @@
 import 'package:dalal_street_client/blocs/subscribe/subscribe_cubit.dart';
 import 'package:dalal_street_client/components/stock_bar.dart';
 import 'package:dalal_street_client/config/get_it.dart';
+import 'package:dalal_street_client/config/log.dart';
 import 'package:dalal_street_client/proto_build/datastreams/Subscribe.pb.dart';
 import 'package:dalal_street_client/proto_build/models/Stock.pb.dart';
 import 'package:dalal_street_client/pages/company_page/components/company_tab_view.dart';
