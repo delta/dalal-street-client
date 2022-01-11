f9f926715e23e9889edf7f760bda68bb6f6e94ae
vpranavkrishna <vpranavkrishna@gmail.com>
2022-01-11 10:00:39 +0530

    [feat]:Created Market Events Bloc

---
+++ lib/blocs/market_events/market_event_event.dart
@@ -0,0 +1,24 @@
+part of 'market_event_bloc.dart';
+
+// ignore: camel_case_types
+abstract class MarketEvent_events extends Equatable {
+  const MarketEvent_events();
+  @override
+  List<Object> get props => [];
+}
+
+class GetMarketEvent extends MarketEvent_events {
+  const GetMarketEvent();
+}
+
+class GetMarketEventFeed extends MarketEvent_events {
+  final SubscriptionId subscriptionId;
+  const GetMarketEventFeed(this.subscriptionId);
+  @override
+  List<Object> get props => [subscriptionId];
+}
+
+class GetMoreMarketEvent extends MarketEvent_events {
+  final int lasteventid;
+  const GetMoreMarketEvent(this.lasteventid);
+}
---
+++ lib/blocs/market_events/market_event_state.dart
@@ -0,0 +1,39 @@
+part of 'market_event_bloc.dart';
+
+abstract class MarketEventState extends Equatable {
+  const MarketEventState();
+  @override
+  List<Object> get props => [];
+}
+
+class MarketEventInitial extends MarketEventState {}
+
+class GetMarketEventSucess extends MarketEventState {
+  final GetMarketEventsResponse marketEventsList;
+  const GetMarketEventSucess(this.marketEventsList);
+  @override
+  List<Object> get props => [marketEventsList];
+}
+
+class GetMarketEventFailure extends MarketEventState {
+  final String error;
+  const GetMarketEventFailure(this.error);
+  @override
+  List<Object> get props => [error];
+}
+
+class SubscriptionToMarketEventSuccess extends MarketEventState {
+  final MarketEventUpdate news;
+  const SubscriptionToMarketEventSuccess(this.news);
+
+  @override
+  List<Object> get props => [news];
+}
+
+class SubscriptionToMarketEventFailed extends MarketEventState {
+  final String subscriptionId;
+  const SubscriptionToMarketEventFailed(this.subscriptionId);
+
+  @override
+  List<Object> get props => [subscriptionId];
+}
