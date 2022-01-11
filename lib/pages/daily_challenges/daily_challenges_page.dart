b9c7a068192402f9a0108f5ba66d8a5619cc81ab
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-01-11 13:07:47 +0530

    [ui]: wrap tabview in a card

--- lib/pages/daily_challenges/daily_challenges_page.dart
+++ lib/pages/daily_challenges/daily_challenges_page.dart
@@ -2,6 +2,7 @@ import 'package:dalal_street_client/blocs/daily_challenges/daily_challenges_page
 import 'package:dalal_street_client/blocs/daily_challenges/single_day_challenges/single_day_challenges_cubit.dart';
 import 'package:dalal_street_client/components/failure_message.dart';
 import 'package:dalal_street_client/pages/daily_challenges/single_day_challenges.dart';
+import 'package:dalal_street_client/theme/colors.dart';
 import 'package:dalal_street_client/utils/range.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -86,20 +87,12 @@ class _DailyChallengesPageBodyState extends State<_DailyChallengesPageBody>
   @override
   build(context) => Column(
         children: [
-          TabBar(
-            controller: _tabController,
-            isScrollable: true,
-            tabs: [
-              for (final day in days)
-                Tab(
-                  text: 'Day $day',
-                  icon: Icon(
-                    (day <= marketDay) ? Icons.lock_open : Icons.lock,
-                  ),
-                )
-            ],
+          const SizedBox(height: 12),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: header(),
           ),
-          const SizedBox(height: 16),
+          const SizedBox(height: 12),
           Expanded(
             child: TabBarView(
               controller: _tabController,
@@ -118,5 +111,42 @@ class _DailyChallengesPageBodyState extends State<_DailyChallengesPageBody>
             ),
           ),
         ],
+      );
+
+  Widget header() => Card(
+        color: background2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              const Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Text(
+                  'Daily Challenges',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TabBar(
+                controller: _tabController,
+                isScrollable: true,
+                tabs: [
+                  for (final day in days)
+                    Tab(
+                      text: 'Day $day',
+                      icon: Icon(
+                        (day <= marketDay) ? Icons.lock_open : Icons.lock,
+                      ),
+                    )
+                ],
+              ),
+            ],
+          ),
+        ),
       );
 }
