793167e9fe9ab5d58ef8caaa86964709aaed6762
dhruv <dhruvkachhadia@gmail.com>
2022-01-16 17:27:53 +0530

    remove unused import

--- lib/pages/mortgage/components/stock_mortgage_item.dart
+++ lib/pages/mortgage/components/stock_mortgage_item.dart
@@ -2,7 +2,6 @@ import 'package:dalal_street_client/config/get_it.dart';
 import 'package:dalal_street_client/constants/constants.dart';
 import 'package:dalal_street_client/constants/format.dart';
 import 'package:dalal_street_client/pages/mortgage/components/mortgage_bottom_sheet.dart';
-import 'package:dalal_street_client/pages/stock_exchange/components/exchange_bottom_sheet.dart';
 import 'package:dalal_street_client/proto_build/models/Stock.pb.dart';
 import 'package:dalal_street_client/streams/global_streams.dart';
 import 'package:dalal_street_client/streams/transformations.dart';
