3a6ec844d8b800ab4559601ee885b4f0c768322e
satyamurti <spd.doddini@gmail.com>
2022-01-21 15:56:00 +0530

    [update]: Removed StockPricesBloc and made requested changes

--- lib/pages/company_page/components/calculate_order_fee.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dalal_street_client/constants/constants.dart';
-
-int calculateOrderFee(int totalPrice) {
-  var orderFee = (ORDER_FEE_RATE * totalPrice);
-  return orderFee.toInt();
-}
