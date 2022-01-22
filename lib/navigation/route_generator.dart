bf18e6e138671b434666e3a36addc3f6caba0d13
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-01-22 11:56:25 +0530

    [nav]: return DalalHome for /home

--- lib/navigation/route_generator.dart
+++ lib/navigation/route_generator.dart
@@ -9,6 +9,7 @@ import 'package:dalal_street_client/blocs/exchange/exchange_cubit.dart';
 import 'package:dalal_street_client/blocs/subscribe/subscribe_cubit.dart';
 import 'package:dalal_street_client/blocs/daily_challenges/daily_challenges_page_cubit.dart';
 import 'package:dalal_street_client/blocs/market_depth/market_depth_bloc.dart';
+import 'package:dalal_street_client/dalal_home.dart';
 import 'package:dalal_street_client/pages/auth/check_mail_page.dart';
 import 'package:dalal_street_client/pages/auth/forgot_password_page.dart';
 import 'package:dalal_street_client/pages/auth/login_page.dart';
@@ -93,7 +94,7 @@ class RouteGenerator {
       // Home Pages
       case '/home':
         if (args is User) {
-          return HomePage(user: args);
+          return DalalHome();
         }
         throw Exception('Invalid user args');
 
