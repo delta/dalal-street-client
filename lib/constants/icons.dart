666664f2a5280d99fd21f5bb6ee23a4661a50e8a
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-01-23 18:36:57 +0530

    format

--- lib/constants/icons.dart
+++ lib/constants/icons.dart
@@ -2,8 +2,8 @@ class AppIcons {
   static const String _iconsDir = 'assets/icon/';
 
   static const String currentPrice = _iconsDir + 'currentprice.svg';
-  static const String alltimeHigh = _iconsDir +'alltimehigh.svg';
-  static const String dayHigh = _iconsDir +'dayhigh.svg';
-  static const String crossWhite = _iconsDir +'cross_white.svg';
-  static const String balance = _iconsDir +'balance.svg';
+  static const String alltimeHigh = _iconsDir + 'alltimehigh.svg';
+  static const String dayHigh = _iconsDir + 'dayhigh.svg';
+  static const String crossWhite = _iconsDir + 'cross_white.svg';
+  static const String balance = _iconsDir + 'balance.svg';
 }
