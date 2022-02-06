7cb3b477ead88aa409669478388dc4a4abe26311
Subiksha Suresh <subikshasuresh@Subikshas-MacBook-Pro.local>
2022-02-06 11:44:38 +0530

    implemented cubit functionality to call previously loaded notification

--- lib/pages/notifications_page.dart
+++ lib/pages/notifications_page.dart
@@ -1,11 +1,11 @@
-import 'package:dalal_street_client/blocs/notifications/notifications_cubit.dart';
+//import 'package:dalal_street_client/blocs/notifications/notifications_cubit.dart';
 import 'package:dalal_street_client/config/get_it.dart';
-import 'package:dalal_street_client/config/log.dart';
+//import 'package:dalal_street_client/config/log.dart';
 import 'package:dalal_street_client/streams/global_streams.dart';
 import 'package:dalal_street_client/theme/colors.dart';
-import 'package:dalal_street_client/utils/snackbar.dart';
+//import 'package:dalal_street_client/utils/snackbar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+//import 'package:flutter_bloc/flutter_bloc.dart';
 
 final notifStream = getIt<GlobalStreams>().notificationStream;
 
