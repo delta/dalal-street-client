085c5768f29f043a89af53200821ee3de80d5c3e
Subiksha Suresh <subikshasuresh@Subikshas-MacBook-Pro.local>
2022-02-08 00:34:23 +0530

    implemented notifications which are being displayed

--- lib/pages/notifications_page.dart
+++ lib/pages/notifications_page.dart
@@ -110,8 +110,8 @@ class _NotifsState extends State<NotifsPage> {
         child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Text(
+            children: [
+              const Text(
                 'Notifications',
                 style: TextStyle(
                   fontSize: 22,
@@ -120,11 +120,205 @@ class _NotifsState extends State<NotifsPage> {
                 ),
                 textAlign: TextAlign.start,
               ),
-              //context.read<NotificationsBloc>().add(GetMoreNotifications(
-              //  notifEvents[notifEvents.length - 1].id - 1)),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
+              notifList()
             ]));
+  }
+
+  Widget notifList() =>
+      BlocBuilder<NotificationsBloc, NotifState>(builder: (context, state) {
+        if (state is GetNotifSuccess) {
+          if (state.notifList.moreExists) {
+            notifEvents.addAll(state.notifList.notifications);
+            if (i == 1) {
+              notifEvents.remove(notifEvents[0]);
+              i++;
+            }
+          }
+          return ListView.separated(
+            shrinkWrap: true,
+            physics: const ScrollPhysics(),
+            itemCount: notifEvents.length,
+            itemBuilder: (context, index) {
+              test.Notification notification = notifEvents[index];
+              int id = notification.id;
+              int userid = notification.userId;
+              String text = notification.text;
+              String createdAt = notification.createdAt;
+              bool isBroadcast = notification.isBroadcast;
+
+              return GestureDetector(
+                  child: notifItem(id, userid, createdAt, isBroadcast),
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            NotifsDetail(id: id, userId: userid, text: text),
+                      )));
+            },
+            separatorBuilder: (context, index) {
+              return const Divider();
+            },
+          );
+        } else if (state is GetNotifFailure) {
+          return Column(
+            children: [
+              const Text('Failed to reach server'),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 100,
+                height: 50,
+                child: OutlinedButton(
+                  onPressed: () => context.read<NotificationsBloc>().add(
+                      GetMoreNotifications(
+                          notifEvents[notifEvents.length - 1].id - 1)),
+                  child: const Text('Retry'),
+                ),
+              ),
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: secondaryColor,
+            ),
+          );
+        }
+      });
+
+  Widget notifItem(
+    int id,
+    int userid,
+    String createdAt,
+    bool islatest,
+  ) {
+    if (!islatest) {
+      return (Container(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    width: (MediaQuery.of(context).size.width - 100) * 0.8,
+                    child: Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        child: Text(id.toString(),
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 15)),
+                      ),
+                      fit: FlexFit.loose,
+                    ),
+                  ),
+                  const SizedBox.square(
+                    dimension: 5,
+                  ),
+                ]),
+          ],
+        ),
+      ));
+    } else {
+      return Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: Flexible(
+                            child: Text(id.toString(),
+                                style: const TextStyle(
+                                    color: Colors.white, fontSize: 15)),
+                            fit: FlexFit.loose),
+                      ),
+                      const SizedBox.square(
+                        dimension: 5,
+                      ),
+                    ]),
+                const SizedBox.square(
+                  dimension: 20,
+                ),
+              ]));
+    }
+  }
+}
+
+class NotifsDetail extends StatelessWidget {
+  final int id;
+  final int userId;
+  final String text;
+
+  const NotifsDetail({
+    Key? key,
+    required this.id,
+    required this.userId,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Container(
+      alignment: Alignment.topCenter,
+      decoration: BoxDecoration(
+          color: background2, borderRadius: BorderRadius.circular(10)),
+      margin: const EdgeInsets.all(10),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox.square(
+              dimension: 5,
+            ),
+            const Padding(
+                padding: EdgeInsets.all(15),
+                child: Text(
+                  'Notifications',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: whiteWithOpacity75),
+                  textAlign: TextAlign.left,
+                )),
+            const Padding(
+              padding: EdgeInsets.all(15),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Flexible(
+                child: Text(
+                  text,
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                fit: FlexFit.loose,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Flexible(
+                child: Text(
+                  text,
+                  textAlign: TextAlign.start,
+                  style: const TextStyle(fontSize: 16, color: lightGray),
+                ),
+                fit: FlexFit.loose,
+              ),
+            )
+          ]),
+    )));
   }
 }
