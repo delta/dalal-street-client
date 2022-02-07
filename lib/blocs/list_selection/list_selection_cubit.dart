f00bae22cf32dd245adf0574b382ee52a9e3ef51
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-02-07 19:26:57 +0530

    [docs]: add docs in ListSelectionCubit

--- lib/blocs/list_selection/list_selection_cubit.dart
+++ lib/blocs/list_selection/list_selection_cubit.dart
@@ -3,9 +3,11 @@ import 'package:equatable/equatable.dart';
 
 part 'list_selection_state.dart';
 
+/// Helper cubit to manage list selection ui
 class ListSelectionCubit extends Cubit<ListSelectionState> {
   ListSelectionCubit() : super(const ListSelectionState(0));
 
+  /// Update the selected index
   void updateSelection(int newIndex) {
     // Only update state if index is changed
     if (newIndex != state.selectedIndex) {
@@ -13,6 +15,7 @@ class ListSelectionCubit extends Cubit<ListSelectionState> {
     }
   }
 
+  /// Stream of selected indices
   Stream<int> get selectedIndexStream =>
       stream.map((state) => state.selectedIndex);
 }
