fc1281a8aca61e11fdd666e3e192c1cc0e02b0a2
simmiggrwl <simmiggrwl@gmail.com>
2022-02-10 16:27:29 +0530

    change bg color for table

--- lib/pages/leaderboard_page/components/table_container.dart
+++ lib/pages/leaderboard_page/components/table_container.dart
@@ -1,35 +1,48 @@
+import 'package:dalal_street_client/theme/colors.dart';
 import 'package:flutter/material.dart';
 
 Widget table(
   List<Map<String, String>> tableData,
 ) =>
     Expanded(
-      child: ListView.builder(
-          itemCount: tableData.length,
-          itemBuilder: (BuildContext context, int index) {
-            return SizedBox(
-              width: double.infinity,
-              child: ListTile(
-                onTap: null,
-                visualDensity: const VisualDensity(vertical: -2),
-                title: Row(
-                  children: <Widget>[
-                    const Padding(padding: EdgeInsets.all(10)),
-                    Expanded(
-                        flex: 1,
-                        child: Text(tableData[index]['rank'].toString(),
-                            style: const TextStyle(fontSize: 16))),
-                    Expanded(
-                        flex: 3,
-                        child: Text(tableData[index]['username'].toString(),
-                            style: const TextStyle(fontSize: 16))),
-                    Expanded(
-                        flex: 2,
-                        child: Text(tableData[index]['totalworth'].toString(),
-                            style: const TextStyle(fontSize: 16))),
-                  ],
-                ),
-              ),
-            );
-          }),
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(10),
+              bottomLeft: Radius.circular(10)),
+          color: background2,
+        ),
+        width: double.infinity,
+        child: Expanded(
+          child: ListView.builder(
+              itemCount: tableData.length,
+              itemBuilder: (BuildContext context, int index) {
+                return SizedBox(
+                  width: double.infinity,
+                  child: ListTile(
+                    onTap: null,
+                    visualDensity: const VisualDensity(vertical: -2),
+                    title: Row(
+                      children: <Widget>[
+                        const Padding(padding: EdgeInsets.all(10)),
+                        Expanded(
+                            flex: 1,
+                            child: Text(tableData[index]['rank'].toString(),
+                                style: const TextStyle(fontSize: 16))),
+                        Expanded(
+                            flex: 3,
+                            child: Text(tableData[index]['username'].toString(),
+                                style: const TextStyle(fontSize: 16))),
+                        Expanded(
+                            flex: 2,
+                            child: Text(
+                                tableData[index]['totalworth'].toString(),
+                                style: const TextStyle(fontSize: 16))),
+                      ],
+                    ),
+                  ),
+                );
+              }),
+        ),
+      ),
     );
--- lib/pages/leaderboard_page/components/table_header.dart
+++ lib/pages/leaderboard_page/components/table_header.dart
@@ -1,25 +1,33 @@
+import 'package:dalal_street_client/theme/colors.dart';
 import 'package:flutter/material.dart';
 
-ListTile tableHeader() {
-  return ListTile(
-      title: Row(
-    children: const <Widget>[
-      Padding(padding: EdgeInsets.all(10)),
-      Expanded(
-        flex: 1,
-        child: Text('Rank',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-      ),
-      Expanded(
-        flex: 3,
-        child: Text('Username',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-      ),
-      Expanded(
-        flex: 2,
-        child: Text('Net Worth',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-      )
-    ],
-  ));
+Widget tableHeader() {
+  return Container(
+    decoration: const BoxDecoration(
+      borderRadius: BorderRadius.only(
+          topRight: Radius.circular(10), topLeft: Radius.circular(10)),
+      color: background2,
+    ),
+    child: ListTile(
+        title: Row(
+      children: const <Widget>[
+        Padding(padding: EdgeInsets.all(10)),
+        Expanded(
+          flex: 1,
+          child: Text('Rank',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        ),
+        Expanded(
+          flex: 3,
+          child: Text('Username',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        ),
+        Expanded(
+          flex: 2,
+          child: Text('Net Worth',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        )
+      ],
+    )),
+  );
 }
