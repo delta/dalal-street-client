7b9d1327ff99748a7605fd69d252011647cec02d
Subiksha Suresh <subikshasuresh@Subikshas-MacBook-Pro.local>
2022-02-11 23:21:11 +0530

    fix:fixed showing error in catch block

--- lib/blocs/notifications_cubit/notifications_cubit.dart
+++ lib/blocs/notifications_cubit/notifications_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:dalal_street_client/config/get_it.dart';
+import 'package:dalal_street_client/constants/error_messages.dart';
 import 'package:dalal_street_client/grpc/client.dart';
 import 'package:dalal_street_client/proto_build/actions/GetNotifications.pb.dart';
 import 'package:equatable/equatable.dart';
@@ -21,7 +22,7 @@ class NotificationsCubit extends Cubit<NotificationsCubitState> {
 
       emit(GetNotifSuccess(notifResponse));
     } catch (e) {
-      emit(GetNotifFailure(e.toString()));
+      emit(const GetNotifFailure(failedToReachServer));
       logger.i('unsuccessful');
     }
   }
