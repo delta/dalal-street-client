55932e30cf28b1cd4319d9808832366401248c1d
Subiksha Suresh <subikshasuresh@Subikshas-MacBook-Pro.local>
2022-02-11 23:54:58 +0530

    fix:deleted unnecessary notifications detail page

--- lib/pages/notifications_page.dart
+++ lib/pages/notifications_page.dart
@@ -1,11 +1,9 @@
 import 'package:dalal_street_client/blocs/notifications_cubit/notifications_cubit.dart';
 import 'package:dalal_street_client/config/get_it.dart';
-
 import 'package:dalal_street_client/proto_build/models/Notification.pb.dart'
     as test;
 import 'package:dalal_street_client/streams/global_streams.dart';
 import 'package:dalal_street_client/theme/colors.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
