11d959d61baec3af3925b889b831d6386abdf5d7
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-02-22 19:20:01 +0530

    [fix]: remove StockBar in MortgagePage

--- lib/pages/mortgage/mortgage_home.dart
+++ lib/pages/mortgage/mortgage_home.dart
@@ -1,5 +1,4 @@
 import 'package:dalal_street_client/blocs/mortgage/mortgage_details/mortgage_details_cubit.dart';
-import 'package:dalal_street_client/components/stock_bar.dart';
 import 'package:dalal_street_client/pages/mortgage/mortgage_page.dart';
 import 'package:dalal_street_client/pages/mortgage/retrieve_page.dart';
 import 'package:dalal_street_client/theme/colors.dart';
@@ -19,34 +18,27 @@ class _MortgageHomeState extends State<MortgageHome> {
     return Scaffold(
       backgroundColor: backgroundColor,
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              const StockBar(),
-              const SizedBox(
-                height: 10,
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Mortgage/Retrieve',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.w700,
-                    color: white,
-                  ),
-                  textAlign: TextAlign.end,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Mortgage/Retrieve',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w700,
+                  color: white,
                 ),
+                textAlign: TextAlign.end,
               ),
-              _mortgageTabView(context),
-            ],
-          ),
+            ),
+            _mortgageTabView(context),
+          ],
         ),
       ),
     );
