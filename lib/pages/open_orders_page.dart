f0ee0e74b9bf8e8bca9fc2850eb0b50b730b653a
dhruv <dhruvkachhadia@gmail.com>
2022-03-02 16:20:47 +0530

    [update]: refactor mobile UI and made it responsive

--- lib/pages/open_orders_page.dart
+++ lib/pages/open_orders_page.dart
@@ -8,6 +8,7 @@ import 'package:dalal_street_client/proto_build/models/Bid.pb.dart';
 import 'package:dalal_street_client/proto_build/models/Stock.pb.dart';
 import 'package:dalal_street_client/streams/global_streams.dart';
 import 'package:dalal_street_client/utils/iso_to_datetime.dart';
+import 'package:dalal_street_client/utils/responsive.dart';
 import 'package:dalal_street_client/utils/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -30,160 +31,206 @@ class _OpenOrdersPageState extends State<OpenOrdersPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                'Open Orders',
-                style: TextStyle(
-                    color: lightGray,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w800),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            body: Container(
-                margin: const EdgeInsets.all(10),
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: background2),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Padding(
-                          padding: EdgeInsets.fromLTRB(5, 10, 10, 5),
-                          child: Align(
-                            child: Text(
-                              'Double click an order to close it',
-                              style: TextStyle(fontSize: 11, color: lightGray),
-                              textAlign: TextAlign.right,
-                            ),
-                            alignment: Alignment.bottomRight,
-                          )),
-                      Center(
-                          child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Expanded(
-                            child: Text(
-                              'ACTION',
-                              style: TextStyle(
-                                color: blurredGray,
-                                fontSize: 11,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              'DETAIL',
-                              style: TextStyle(
-                                color: blurredGray,
-                                fontSize: 11,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              'STATUS',
-                              style: TextStyle(
-                                color: blurredGray,
-                                fontSize: 11,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          )
-                        ],
-                      )),
-                      Expanded(
-                        child: BlocConsumer<MyOrdersCubit, MyOrdersState>(
-                            listener: (context, state) {
-                          if (state is CancelOrderSuccess) {
-                            showSnackBar(
-                                context, 'Order Cancelled Successfully',
-                                type: SnackBarType.success);
-                            context.read<MyOrdersCubit>().getMyOpenOrders();
-                          } else if (state is CancelOrderFailure) {
-                            showSnackBar(context, state.error,
-                                type: SnackBarType.error);
-                          }
-                        }, builder: (context, state) {
-                          if (state is OpenOrdersSuccess) {
-                            final askOrders = state.openAskOrders;
-                            final bidOrders = state.openBidOrders;
-                            List<Widget> openOrders =
-                                buildList(askOrders, bidOrders);
-
-                            return SingleChildScrollView(
-                              child: Column(children: [
-                                ListView.separated(
-                                  shrinkWrap: true,
-                                  physics: const NeverScrollableScrollPhysics(),
-                                  itemCount: openOrders.length,
-                                  itemBuilder: (context, index) {
-                                    bool isAsk = index < askOrders.length;
-                                    int orderId = isAsk
-                                        ? askOrders[index].id
-                                        : bidOrders[index - askOrders.length]
-                                            .id;
-                                    return GestureDetector(
-                                      child: openOrders[index],
-                                      onDoubleTap: () {
-                                        context
-                                            .read<MyOrdersCubit>()
-                                            .cancelMyOrder(isAsk, orderId);
-                                      },
-                                    );
-                                  },
-                                  separatorBuilder: (context, index) =>
-                                      const Divider(color: lightGray),
-                                ),
-                                const Divider(
-                                  color: lightGray,
-                                ),
-                              ]),
-                            );
-                          } else if (state is NoOpenOrders) {
-                            return (const Center(
-                              child: Padding(
-                                  padding: EdgeInsets.all(10),
-                                  child: Text('No Open Orders',
-                                      style: TextStyle(
-                                          fontSize: 14, color: Colors.white))),
-                            ));
-                          } else if (state is OpenOrdersFailure) {
-                            return Center(
-                                child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text('Failed to Reach the Server'),
-                                const SizedBox(height: 20),
-                                SizedBox(
-                                  width: 100,
-                                  height: 50,
-                                  child: OutlinedButton(
-                                    onPressed: () {
-                                      context
-                                          .read<MyOrdersCubit>()
-                                          .getMyOpenOrders();
-                                    },
-                                    child: const Text('Retry'),
-                                  ),
-                                ),
-                              ],
-                            ));
-                          } else {
-                            return const Center(
-                              child: DalalLoadingBar(),
-                            );
-                          }
-                        }),
-                      )
-                    ]))));
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: backgroundColor,
+        body: SingleChildScrollView(
+            primary: false,
+            physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics()),
+            child: Responsive(
+              desktop: _desktopBody(),
+              mobile: _mobileBody(),
+              tablet: _tabletBody(),
+            )),
+      ),
+    );
   }
+
+  Widget _desktopBody() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          _openOrdersWeb(),
+          const SizedBox(
+            height: 10,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _openOrdersWeb() => const Text('hello');
+
+  Widget _mobileBody() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          _openOrdersMobile(),
+          const SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _openOrdersMobile() => Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: background2),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Open Orders',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: lightGray,
+              ),
+              textAlign: TextAlign.end,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(5, 0, 10, 0),
+                child: Align(
+                  child: Text(
+                    'Double click an order to close it',
+                    style: TextStyle(fontSize: 11, color: lightGray),
+                    textAlign: TextAlign.right,
+                  ),
+                  alignment: Alignment.bottomRight,
+                )),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Expanded(
+                  child: Text(
+                    'ACTION',
+                    style: TextStyle(
+                      color: blurredGray,
+                      fontSize: 11,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'DETAIL',
+                    style: TextStyle(
+                      color: blurredGray,
+                      fontSize: 11,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'STATUS',
+                    style: TextStyle(
+                      color: blurredGray,
+                      fontSize: 11,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ],
+            ),
+            BlocConsumer<MyOrdersCubit, MyOrdersState>(
+                listener: (context, state) {
+              if (state is CancelOrderSuccess) {
+                showSnackBar(context, 'Order Cancelled Successfully',
+                    type: SnackBarType.success);
+                context.read<MyOrdersCubit>().getMyOpenOrders();
+              } else if (state is CancelOrderFailure) {
+                showSnackBar(context, state.error, type: SnackBarType.error);
+              }
+            }, builder: (context, state) {
+              if (state is OpenOrdersSuccess) {
+                final askOrders = state.openAskOrders;
+                final bidOrders = state.openBidOrders;
+                List<Widget> openOrders = buildList(askOrders, bidOrders);
+
+                return Column(children: [
+                  ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: openOrders.length,
+                    itemBuilder: (context, index) {
+                      bool isAsk = index < askOrders.length;
+                      int orderId = isAsk
+                          ? askOrders[index].id
+                          : bidOrders[index - askOrders.length].id;
+                      return GestureDetector(
+                        child: openOrders[index],
+                        onDoubleTap: () {
+                          context
+                              .read<MyOrdersCubit>()
+                              .cancelMyOrder(isAsk, orderId);
+                        },
+                      );
+                    },
+                    separatorBuilder: (context, index) =>
+                        const Divider(color: lightGray),
+                  ),
+                  const Divider(
+                    color: lightGray,
+                  ),
+                ]);
+              } else if (state is NoOpenOrders) {
+                return (const Center(
+                  child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text('No Open Orders',
+                          style:
+                              TextStyle(fontSize: 14, color: Colors.white))),
+                ));
+              } else if (state is OpenOrdersFailure) {
+                return Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Failed to Reach the Server'),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: 100,
+                      height: 50,
+                      child: OutlinedButton(
+                        onPressed: () {
+                          context.read<MyOrdersCubit>().getMyOpenOrders();
+                        },
+                        child: const Text('Retry'),
+                      ),
+                    ),
+                  ],
+                ));
+              } else {
+                return const Center(
+                  child: DalalLoadingBar(),
+                );
+              }
+            })
+          ]));
 
   List<Widget> buildList(List<Ask> askOrders, List<Bid> bidOrders) =>
       askOrders.map((e) => buildItem(e, OpenOrderType.ASK)).toList() +
@@ -202,7 +249,7 @@ class _OpenOrdersPageState extends State<OpenOrdersPage> {
             children: [
               ClipRRect(
                   child: Padding(
-                      padding: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.fromLTRB(0,10,10,10),
                       child: Image.network(
                         'https://assets.airtel.in/static-assets/new-home/img/favicon-32x32.png',
                         height: 25,
@@ -214,12 +261,16 @@ class _OpenOrdersPageState extends State<OpenOrdersPage> {
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const SizedBox.square(dimension: 20),
-                      Text(
-                        (company?.fullName.toUpperCase())!,
-                        style: const TextStyle(fontSize: 12, color: lightGray),
-                        textAlign: TextAlign.left,
-                        maxLines: 2,
+                      const SizedBox.square(dimension: 10),
+                      SizedBox(
+                        width: 80,
+                        child: Text(
+                          (company?.fullName.toUpperCase())!,
+                          style:
+                              const TextStyle(fontSize: 12, color: lightGray),
+                          textAlign: TextAlign.left,
+                          maxLines: 2,
+                        ),
                       ),
                       const SizedBox.square(dimension: 10),
                       Text(
@@ -227,14 +278,16 @@ class _OpenOrdersPageState extends State<OpenOrdersPage> {
                         style: const TextStyle(fontSize: 12),
                         textAlign: TextAlign.left,
                       ),
+                      const SizedBox.square(dimension: 10),
                     ]),
+                    
               ),
               Expanded(
                 flex: 1,
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const SizedBox.square(dimension: 20),
+                      const SizedBox.square(dimension: 10),
                       const Text(
                         'TYPE',
                         style: TextStyle(fontSize: 9, color: lightGray),
@@ -259,6 +312,7 @@ class _OpenOrdersPageState extends State<OpenOrdersPage> {
                         ),
                         maxLines: 1,
                       ),
+                      const SizedBox.square(dimension: 10),
                     ]),
               ),
               Expanded(
@@ -266,7 +320,7 @@ class _OpenOrdersPageState extends State<OpenOrdersPage> {
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      const SizedBox.square(dimension: 20),
+                      const SizedBox.square(dimension: 10),
                       const Text(
                         'COMPLETED',
                         style: TextStyle(fontSize: 9, color: lightGray),
@@ -287,9 +341,22 @@ class _OpenOrdersPageState extends State<OpenOrdersPage> {
                             style: const TextStyle(
                                 fontSize: 12, color: Colors.white),
                             textAlign: TextAlign.right,
-                          ))
+                          )),
+                      const SizedBox.square(dimension: 10),    
                     ]),
               )
             ]));
+  }
+
+  Center _tabletBody() {
+    return const Center(
+      child: Text(
+        'Tablet UI will design soon :)',
+        style: TextStyle(
+          fontSize: 14,
+          color: secondaryColor,
+        ),
+      ),
+    );
   }
 }
