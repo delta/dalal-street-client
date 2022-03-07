15c0b47d9fea581bc463cfd98d33dc5c3e97f45c
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-03-07 13:12:30 +0530

    [fix]: exception in log filter

--- lib/config/log.dart
+++ lib/config/log.dart
@@ -5,28 +5,37 @@ import 'package:logger/logger.dart';
 ///
 /// for more configurations, checkout https://pub.dev/packages/logger
 final Logger logger = Logger(
-  filter: _filter,
+  filter: DalalLogFilter(),
   printer: PrettyPrinter(
     colors: true,
     printEmojis: true,
   ),
 );
 
-final _filter = DalalLogFilter();
-
+/// Custom log filter, which gives default behaviour in debug mode, and shows
+/// only non-debug logs in release mode
+///
 /// see https://pub.dev/packages/logger#logfilter
 class DalalLogFilter extends LogFilter {
   @override
   bool shouldLog(LogEvent event) {
+    if (!kReleaseMode) {
+      return _defaultShouldLog(event);
+    }
+    if (event.level != Level.debug) {
+      return true;
+    }
+    return false;
+  }
+
+  /// From https://pub.dev/documentation/logger/1.1.0/logger/DevelopmentFilter/shouldLog.html
+  bool _defaultShouldLog(LogEvent event) {
     var shouldLog = false;
     assert(() {
-      if (!kReleaseMode) {
-        shouldLog = DevelopmentFilter().shouldLog(event);
-      }
-      if (event.level != Level.debug) {
+      if (event.level.index >= level!.index) {
         shouldLog = true;
       }
-      return false;
+      return true;
     }());
     return shouldLog;
   }
