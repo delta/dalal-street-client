fa485d5130f6b72b5f50635214aef82b013ab892
siva2204 <sivaraman26r@gmail.com>
2022-03-10 11:14:54 +0530

    [Add] margin for web

--- lib/blocs/daily_challenges/daily_challenges_page_cubit.dart
+++ lib/blocs/daily_challenges/daily_challenges_page_cubit.dart
@@ -4,12 +4,19 @@ import 'package:dalal_street_client/config/log.dart';
 import 'package:dalal_street_client/constants/error_messages.dart';
 import 'package:dalal_street_client/grpc/client.dart';
 import 'package:dalal_street_client/proto_build/actions/GetDailyChallengeConfig.pb.dart';
+import 'package:dalal_street_client/streams/global_streams.dart';
+import 'package:dalal_street_client/streams/transformations.dart';
 import 'package:equatable/equatable.dart';
 
 part 'daily_challenges_page_state.dart';
 
 class DailyChallengesPageCubit extends Cubit<DailyChallengesPageState> {
   DailyChallengesPageCubit() : super(DailyChallengesPageLoading());
+
+  Stream<bool> get isOpenStream => getIt<GlobalStreams>()
+      .gameStateStream
+      .isDailyChallengesOpenStream()
+      .distinct();
 
   Future<void> getChallengesConfig() async {
     try {
@@ -23,6 +30,13 @@ class DailyChallengesPageCubit extends Cubit<DailyChallengesPageState> {
           resp.marketDay,
           resp.totalMarketDays,
         ));
+
+        isOpenStream.listen((event) {
+          if (event) {
+            emit(DailyChallengesPageSuccess(
+                true, resp.marketDay + 1, resp.totalMarketDays));
+          }
+        });
       } else {
         emit(DailyChallengesPageFailure(resp.statusMessage));
       }
