65b42defd506487e26058f7bb4c24b595f5ba613
siva2204 <sivaraman26r@gmail.com>
2022-03-10 20:06:07 +0530

    [remove] background color

--- lib/pages/mediapartners_page.dart
+++ lib/pages/mediapartners_page.dart
@@ -13,17 +13,9 @@ class MediaPartners extends StatelessWidget {
                     child: Container(
                         width: MediaQuery.of(context).size.width,
                         margin: const EdgeInsets.all(25),
-                        decoration: BoxDecoration(
-                            color: background2,
-                            borderRadius: BorderRadius.circular(10)),
                         child: Column(
                           children: [
-                            const SizedBox.square(dimension: 10),
-                            const Text(
-                              'Media Partners',
-                              style: TextStyle(fontSize: 24, color: white),
-                            ),
-                            const SizedBox.square(dimension: 20),
+                            const SizedBox.square(dimension: 15),
                             Card(
                               elevation: 10,
                               child: Padding(
