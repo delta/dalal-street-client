1052983ac4cbfeb3a9a4df7ea22f820a804822bd
kelpikz <ajithaprasad96@gmail.com>
2022-03-10 17:19:05 +0530

    feat(landing): landing page web ui

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,3 +1,7 @@
 const deltaUrl = 'https://delta.nitt.edu/';
 
 const helikopterHelikopter = 'https://www.youtube.com/watch?v=3ExGuHWdXCE';
+
+const docsLink = 'https://docs.dalal.pragyan.org';
+
+const forumLink = 'https://discord.gg';
