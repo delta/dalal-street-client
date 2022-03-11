913b2d4a8b4cfce9e22f8938b9264afd8c902725
dhruv <dhruvkachhadia@gmail.com>
2022-03-11 12:12:05 +0530

    [fix]: referral page parent widget bug

--- lib/pages/referral_page.dart
+++ lib/pages/referral_page.dart
@@ -17,87 +17,86 @@ class ReferralPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child:
-          BlocBuilder<ReferralCubit, ReferralState>(builder: (context, state) {
-        if (state is ReferralSuccess) {
-          return referralPageUi(!showbutton, state.referralCode, context);
-        } else if (state is ReferralFailed) {
-          return Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Failed to Fetch the Referral Code'),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: 100,
-                height: 50,
-                child: OutlinedButton(
-                  onPressed: () {
-                    context.read<ReferralCubit>().getReferralcode(user.email);
-                  },
-                  child: const Text('Retry'),
+    return SafeArea(
+      child: Scaffold(body: Center(
+        child:
+            BlocBuilder<ReferralCubit, ReferralState>(builder: (context, state) {
+          if (state is ReferralSuccess) {
+            return referralPageUi(!showbutton, state.referralCode, context);
+          } else if (state is ReferralFailed) {
+            return Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Failed to Fetch the Referral Code'),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 100,
+                  height: 50,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      context.read<ReferralCubit>().getReferralcode(user.email);
+                    },
+                    child: const Text('Retry'),
+                  ),
                 ),
-              ),
-            ],
-          ));
-        } else {
-          return referralPageUi(showbutton, null, context);
-        }
-      }),
-    ));
+              ],
+            ));
+          } else {
+            return referralPageUi(showbutton, null, context);
+          }
+        }),
+      )),
+    );
   }
 
   Widget referralPageUi(
       bool showbutton, String? referralcode, BuildContext context) {
-    return SafeArea(
-        child: Center(
+    return Center(
       child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Stack(children: <Widget>[
-              Positioned(
-                child: Image.asset('assets/images/Background.png'),
-              ),
-              Positioned(
-                child: Image.asset('assets/images/Referral.png'),
-                left: 60,
-                top: 50,
-              )
-            ]),
-            const Padding(
-                padding: EdgeInsets.fromLTRB(0, 40, 0, 40),
-                child: Text(
-                  'Referral Code',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                )),
-            const Padding(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                child: Flexible(
-                    child: Text(
-                  'Refer your friends to earn a cash reward',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18),
-                ))),
-            const SizedBox(height: 50),
-            buttonWidget(showbutton, referralcode, context),
-            const Padding(
-                padding: EdgeInsets.fromLTRB(0, 40, 0, 10),
-                child: Text('How it works ?',
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
-            const Flexible(
-                child: Text(
-              'Refer your friends to Dalal Street, and if they sign up, both of you will get a cash reward.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.normal,
-              ),
-            ))
-          ]),
-    ));
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Stack(children: <Widget>[
+          Positioned(
+            child: Image.asset('assets/images/Background.png'),
+          ),
+          Positioned(
+            child: Image.asset('assets/images/Referral.png'),
+            left: 60,
+            top: 50,
+          )
+        ]),
+        const Padding(
+            padding: EdgeInsets.fromLTRB(0, 40, 0, 40),
+            child: Text(
+              'Referral Code',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            )),
+        const Flexible(
+            child: Text(
+          'Refer your friends to earn a cash reward',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+        )),
+        const SizedBox(height: 50),
+        buttonWidget(showbutton, referralcode, context),
+        const Padding(
+            padding: EdgeInsets.fromLTRB(0, 40, 0, 10),
+            child: Text('How it works ?',
+                style:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+        const Flexible(
+            child: Text(
+          'Refer your friends to Dalal Street, and if they sign up, both of you will get a cash reward.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.normal,
+          ),
+        ))
+      ]),
+    );
   }
 
   Widget buttonWidget(
@@ -149,10 +148,9 @@ class ReferralPage extends StatelessWidget {
             onPressed: () {
               context.read<ReferralCubit>().getReferralcode(user.email);
             },
-            child: const Expanded(
-                child: Text('Generate Referral Code',
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)))),
+            child: const Text('Generate Referral Code',
+                style:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
       );
     }
   }
