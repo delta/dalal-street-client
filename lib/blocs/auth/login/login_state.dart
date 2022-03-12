7a7f682829ae72174749299cecd77b54d7823ce3
siva2204 <sivaraman26r@gmail.com>
2022-03-12 09:22:00 +0530

    [Fix] mail redirect and transaction page bug

--- lib/blocs/auth/login/login_state.dart
+++ lib/blocs/auth/login/login_state.dart
@@ -31,3 +31,13 @@ class LoginSuccess extends LoginState {
   @override
   List<Object> get props => [loginResponse];
 }
+
+class EmailNotVerified extends LoginState {
+  final String msg;
+  final String email;
+
+  const EmailNotVerified(this.msg, this.email);
+
+  @override
+  List<Object> get props => [msg, email];
+}
