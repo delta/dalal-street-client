9970d6310f89249fbe45c2ec192a5e291e6d9f68
siva <63258524+siva2204@users.noreply.github.com>
2022-03-14 11:22:47 +0530

    [update]: Add Discord Link in FAQ page (#133)
    * [update] faq
    
    * [Add] discord links

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -4,4 +4,4 @@ const helikopterHelikopter = 'https://www.youtube.com/watch?v=3ExGuHWdXCE';
 
 const docsLink = 'https://dalal-help-page.vercel.app/';
 
-const forumLink = 'https://discord.gg/FmkBdXSjnj/';
+const forumLink = 'https://discord.gg/FmkBdXSjnj';
