727a8b4d3b2079cf27866099d1f817d28609b177
Satyamurti Doddini <59143264+satyamurti@users.noreply.github.com>
2022-03-16 18:04:39 +0530

    [fix]: Fixed padding issue in notifications (#141)
    * [fix]: Fixed padding issue in notifications
    
    * fix: delete unnecessary files
    
    Co-authored-by: Ajitha Prasad Y <59395177+kelpikz@users.noreply.github.com>

--- lib/pages/notifications_page.dart
+++ lib/pages/notifications_page.dart
@@ -172,27 +172,27 @@ class _NotificationsState extends State<NotificationsPage> {
                         ),
                       ),
                       const SizedBox(width: 20),
-                      SizedBox(
-                        width: screenWidth > 1000
-                            ? screenWidth * 0.7
-                            : screenWidth * 0.5,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(text,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            width: screenWidth * 0.7,
+                            child: Text(text,
                                 style: const TextStyle(
                                     color: Colors.white, fontSize: 15)),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Container(
-                              alignment: Alignment.bottomRight,
-                              child: Text(ISOtoDateTime(createdAt),
-                                  textAlign: TextAlign.right,
-                                  style: TextStyle(color: iconColor)),
-                            ),
-                          ],
-                        ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Text(ISOtoDateTime(createdAt),
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(color: iconColor)),
+                              ]),
+                        ],
                       ),
                     ],
                   ),
